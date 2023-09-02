4fca71260042b4007c1b3213a3bbe6468eb449b2
Reyghita Hafizh <87755064+reyyghi@users.noreply.github.com>
2023-09-02 10:29:04 +0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,16 +5,19 @@ atuhor 'Reyghita Hafizh & Della'
 lua54 'yes'
 
 client_scripts {
+    'bridge/**/cl_main.lua',
     'client/cl_*.lua'
 }
 
 shared_scripts {
     '@ox_lib/init.lua',
+    'bridge/shared.lua',
     'shared/sh_*.lua'
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
+    'bridge/**/sv_main.lua',
     'server/sv_*.lua'
 }
 
