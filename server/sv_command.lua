6a68c2862cb3e2d6ff679e4fb08962a4dffcf9ad
reyyghi <reyhafizh07@gmail.com>
2024-02-25 23:26:51 +0700

    update sv_command

--- server/sv_command.lua
+++ server/sv_command.lua
@@ -1,6 +1,13 @@
-lib.addCommand('buatgudang', {
-    help = 'Untuk membuat gudang baru (Hanya Admin)',
-    restricted = 'group.admin'
-}, function(source, args, raw)
-    TriggerClientEvent('rhd_os:client:createstash', source)
+local command = require 'config.command'
+
+CreateThread(function ()
+    for index, data in pairs(command) do
+        RegisterCommand(data.name, function (playerId)
+            Perms.get(playerId, function (authorized)
+                if authorized then
+                    TriggerClientEvent(data.event, playerId)
+                end
+            end)
+        end, false)
+    end
 end)
