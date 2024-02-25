ccee4122a2fd6a29efaf491a631be391d0e5c947
reyyghi <reyhafizh07@gmail.com>
2024-02-25 22:06:42 +0700

    added prop & stash data

---
+++ data/stash.lua
@@ -0,0 +1,18 @@
+return {
+    ["rhd_storage"] = {
+        id = "rhd_storage",
+        label = "RHD Storage",
+        slots = 20,
+        weight = 250,
+        coords = vec(812.73, -1340.41, 25.27, 0.00),
+        prop = "v_res_fh_sidebrdlngb"
+    },
+    ["awd"] = {
+        id = "awd",
+        label = "adwa",
+        slots = 34,
+        weight = 34,
+        coords = vec(815.70, -1343.21, 26.38, 0.00),
+    },
+
+}
