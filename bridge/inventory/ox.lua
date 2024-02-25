4ea87e72520eed9d0da65ba8679768fc9e834198
reyyghi <reyhafizh07@gmail.com>
2024-02-25 23:26:06 +0700

    bridge/inventory/ox: added registerstash when resources started

--- bridge/inventory/ox.lua
+++ bridge/inventory/ox.lua
@@ -11,4 +11,15 @@ if isServer then
     function RB.i.registerStash(stashData)
         inventory:RegisterStash(stashData.id, stashData.label, stashData.slots, stashData.weight, stashData.owned)
     end
+    
+    AddEventHandler('onResourceStart', function(resource)
+        if resource == GetCurrentResourceName() then
+            local stash = Storage.getData()
+            if stash and next(stash) then
+                for id, stashData in pairs(stash) do
+                    RB.i.registerStash(stashData)
+                end
+            end
+        end
+    end)
 end
