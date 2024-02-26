902e62e113c5ed18fbf5528aeb6fc801d9904e7e
reyyghi <reyhafizh07@gmail.com>
2024-02-26 07:46:02 +0700

    update stash

--- data/stash.lua
+++ data/stash.lua
@@ -1,18 +1,37 @@
 return {
-    ["rhd_storage"] = {
-        id = "rhd_storage",
-        label = "RHD Storage",
-        slots = 20,
-        weight = 250,
-        coords = vec(812.73, -1340.41, 25.27, 0.00),
-        prop = "v_res_fh_sidebrdlngb"
+    ["awdwadwadwadwa"] = {
+        id = "awdwadwadwadwa",
+        label = "dawdwadwa",
+        slots = 343,
+        weight = 343000,
+        owned = false,
+        coords = vec(815.75, -1339.80, 26.50, 0.00),
+        rentalData = {
+            price = 34,
+            identifier = {["928V5582"] = "27/02/2024"}
+        },
     },
-    ["awd"] = {
-        id = "awd",
-        label = "adwa",
+    ["gudangbaru"] = {
+        id = "gudangbaru",
+        label = "Gudang Baru",
+        slots = 234,
+        weight = 234000,
+        owned = true,
+        coords = vec(815.71, -1342.75, 26.22, 0.00),
+        rentalData = {
+            price = 5000,
+            identifier = {["928V5582"] = "28/02/2024"}
+        },
+    },
+    ["awdawdwadwadwadwad"] = {
+        id = "awdawdwadwadwadwad",
+        label = "awda",
         slots = 34,
-        weight = 34,
-        coords = vec(815.70, -1343.21, 26.38, 0.00),
+        weight = 34000,
+        owned = true,
+        groups = {police = 2},
+        coords = vec(278.03, -612.39, 42.19, 0.00),
+        prop = "v_ind_rc_lockeropn",
     },
 
 }
